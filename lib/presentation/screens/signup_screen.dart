5649779ccc3375bcb49299e0a0a053de802297ce
DucThanh <phoducthanh@gmail.com>
2025-02-17 13:40:19 +0700

    date picker in register

--- lib/presentation/screens/signup_screen.dart
+++ lib/presentation/screens/signup_screen.dart
@@ -1,7 +1,14 @@
-import 'package:android_hms/core/services/Auth/api_signUp.dart';
 import 'package:flutter/material.dart';
+import 'package:android_hms/core/services/Auth/api_signUp.dart';
 
-class SignupScreen extends StatelessWidget {
+class SignupScreen extends StatefulWidget {
+  SignupScreen({super.key});
+
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController firstNameController = TextEditingController();
   final TextEditingController lastNameController = TextEditingController();
   final TextEditingController phoneNumberController = TextEditingController();
@@ -13,20 +20,44 @@ class SignupScreen extends StatelessWidget {
   final TextEditingController reEnterPasswordController =
       TextEditingController();
 
-  SignupScreen({super.key});
+  DateTime? selectedDate; // Lưu trữ ngày đã chọn
+
+  Future<void> _selectDate(BuildContext context) async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+    if (pickedDate != null) {
+      setState(() {
+        selectedDate = pickedDate;
+        dateOfBirthController.text =
+            "${pickedDate.day.toString().padLeft(2, '0')}/"
+            "${pickedDate.month.toString().padLeft(2, '0')}/"
+            "${pickedDate.year}";
+      });
+    }
+  }
 
   Future<void> registerUser(BuildContext context) async {
     final firstName = firstNameController.text;
     final lastName = lastNameController.text;
     final phoneNumber = phoneNumberController.text;
     final citizenIdentity = citizenIdentityController.text;
-    final dateOfBirth = dateOfBirthController.text;
     final email = emailController.text;
     final password = passwordController.text;
     final reEnterPassword = reEnterPasswordController.text;
+
     if (password == reEnterPassword) {
+      // Chuyển đổi ngày sinh sang 'YYYY-MM-DD'
+      final dateOfBirth = selectedDate != null
+          ? "${selectedDate!.year}-${selectedDate!.month.toString().padLeft(2, '0')}-${selectedDate!.day.toString().padLeft(2, '0')}"
+          : '';
+
       final response = await ApiSignup.signUp(email, password, firstName,
           lastName, phoneNumber, citizenIdentity, dateOfBirth);
+
       if (response == 200) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Registration successful!')),
@@ -117,9 +148,11 @@ class SignupScreen extends StatelessWidget {
               TextField(
                 controller: dateOfBirthController,
                 decoration: InputDecoration(
-                  labelText: 'ngày/tháng/năm sinh',
+                  labelText: 'Ngày/tháng/năm sinh',
                   border: OutlineInputBorder(),
                 ),
+                readOnly: true,
+                onTap: () => _selectDate(context),
               ),
               SizedBox(height: 20),
               ElevatedButton(
