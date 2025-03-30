c092f1548731cf01e9559044bce1f810d9364775
DanhHieuuuuu <hieunguyendanh91@gmail.com>
2025-03-30 22:30:18 +0700

    chức năng chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:android_hms/Data/favourite_provider.dart';
 import 'package:android_hms/Data/voucher_provider.dart';
 import 'package:android_hms/presentation/screens/room_detail_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,8 +15,11 @@ import 'package:android_hms/presentation/screens/signup_screen.dart';
 import 'package:android_hms/presentation/screens/home_screen_bottom.dart';
 import 'package:android_hms/presentation/screens/forgot_password_screen.dart';
 import 'package:android_hms/presentation/screens/change_password_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (_) => HotelProvider()),
