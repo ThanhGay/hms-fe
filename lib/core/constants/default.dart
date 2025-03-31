fc34693e7a128ba1d52a9436d113dd37954ee5a9
DucThanh <phoducthanh@gmail.com>
2025-03-31 09:54:39 +0700

    create default file

---
+++ lib/core/constants/default.dart
@@ -0,0 +1,5 @@
+class DefaultConstants {
+  final String defaultImageRoom = "images/default/room.jpg";
+  final String defaultAvatarUser = "images/default/avatar.png";
+  final String defaultImageHotel = "images/default/hotel.jpg";
+}
